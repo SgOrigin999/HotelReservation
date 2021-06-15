1dcc2f6469c5a8210074de9a469c18da1d3522f0
SgOrigin999 <you@example.com>
2021-06-15 18:45:29 +0530

    Welcome To HotelReservationSystem

---
+++ com/bridgelab/hrs/HotelReservation.java
@@ -0,0 +1,9 @@
+package com.bridgelab.hrs;
+
+public class HotelReservation {
+	
+	public HotelReservation() {
+		super();
+		System.out.println("Welcome To Hotel Reservation System::");
+	}
+}
---
+++ com/bridgelab/hrs/HotelReservationTest.java
@@ -0,0 +1,11 @@
+package com.bridgelab.hrs;
+
+import org.junit.jupiter.api.Test;
+
+public class HotelReservationTest {
+	
+	@Test
+	public void welcomeMessage() {
+		HotelReservation hotlData=new HotelReservation();
+	}
+}
